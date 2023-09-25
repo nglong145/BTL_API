b2161edff1a712f9f1cd5a508a423c1d5d811457
Acer <Acer@DESKTOP-F9TGDL5>
2023-09-25 10:44:37 +0700

    Add project files.

---
+++ DataAccessLayer/TaiKhoanRepository.cs
@@ -0,0 +1,35 @@
+﻿using DataModel;
+
+namespace DataAccessLayer
+{
+    public class TaiKhoanRepository : ITaiKhoanRepository
+    {
+        private IDatabaseHelper _dbHelper;
+        public TaiKhoanRepository(IDatabaseHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+        public bool Create(TaiKhoanModel model)
+        {
+            string msgError = "";
+            try
+            {
+                var xxx = model.list_json_chitiethoadon != null ? MessageConvert.SerializeObject(model.list_json_chitiethoadon) : null;
+                var result = _dbHelper.ExecuteScalarSProcedureWithTransaction(out msgError, "sp_hoadon_create",
+                "@TenKH", model.TenKH,
+                "@Diachi", model.Diachi,
+                "@TrangThai", model.TrangThai,
+                "@list_json_chitiethoadon", model.list_json_chitiethoadon != null ? MessageConvert.SerializeObject(model.list_json_chitiethoadon) : null);
+                if ((result != null && !string.IsNullOrEmpty(result.ToString())) || !string.IsNullOrEmpty(msgError))
+                {
+                    throw new Exception(Convert.ToString(result) + msgError);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ DataModel/TaiKhoanModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class TaiKhoanModel
+    {
+        public int MaTaiKhoan { get; set; }
+        public int LoaiTaiKhoan { get; set; }
+        public string TenTaiKhoan { get; set; }
+        public string MatKhau { get; set; }
+        public string Eamil { get; set; }
+
+    }
+
+
+}
